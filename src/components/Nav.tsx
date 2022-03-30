9d63cea7096dd00f0d959ba5e458a969b7f45be4
jrkozak92 <jrkozak92@gmail.com>
2022-03-30 13:28:28 -0500

    commiting to get to Main to pull to push to heroku

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,8 +2,18 @@ import { useState, useEffect } from 'react'
 import Add from './Add'
 
 const Nav = () => {
+  const [dropdown, setDropdown] = useState('')
+
+
+
   return (
-    <Add/>
+    <div>
+    { dropdown === 'add' ?
+      <Add/>
+        :
+      Add
+    }
+    </div>
   )
 }
 
